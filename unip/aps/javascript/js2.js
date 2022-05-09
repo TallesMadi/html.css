80781ddd265242d531c8943d4e27178f5dae63ba
TallesMadi <102757246+TallesMadi@users.noreply.github.com>
2022-05-09 16:58:56 -0300

    aps novas funcoes

--- unip/aps/javascript/js2.js
+++ unip/aps/javascript/js2.js
@@ -1,15 +1,15 @@
 function lista() {
     var lista = window.document.querySelector('div#lista')
        
-       if (lista.style.overflow == "inherit") {
-           lista.style.height = "0"
-           lista.style.opacity = "0"
-           lista.style.overflow = "hidden"
-           
-       } else {
+       if (lista.style.opacity == 0) {
             lista.style.height = "fit-content"
             lista.style.opacity = "1"
             lista.style.overflow = "inherit"
+           
+       } else {
+            lista.style.height = "0"
+            lista.style.opacity = "0"
+            lista.style.overflow = "hidden"
        }
    }
 
